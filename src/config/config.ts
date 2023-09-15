87bd930c3616c0aa3c05657d1a6a9611ee59f0d1
Hernando Clareth <hernando.ariza@ormuco.com>
2023-09-15 10:37:48 -0500

    fixing GetTunnedDucument method

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,11 @@
 
 
 export const CONFIGS = {
-company: {name: "EMPRESA BUENA", logoUrl: "https://logo.png"},
-pdfTitle : "Document",
-s3BaseUrl: `${process.env.S3_CLOUD_ENDPOINT}/swift/v1/1e325b6357b7498d909f5f789f02df29/${process.env.BUCKET_NAME}`
+    company: { name: "EMPRESA BUENA", logoUrl: "https://logo.png" },
+    pdfTitle: "Document",
+    s3BaseUrl: `${process.env.S3_CLOUD_ENDPOINT}/swift/v1/1e325b6357b7498d909f5f789f02df29/${process.env.BUCKET_NAME}`,
+    searchableFields: {
+        beneficiary: ['fist_name', 'last_name'],
+        activity: ['name']
+    }
 };
