c3839c5c3d87beacfe2cf9cfc3ec51ce779f0a11
Hernando Clareth <hernando.ariza@ormuco.com>
2023-09-20 10:31:40 -0500

    fixing routing

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -6,6 +6,8 @@ import usersRouter from './users';
 import beneficiariesRouter from './beneficiaries';
 import activitiesRouter from './activities';
 import rolesRouter from './roles';
+import workshopsRouter from './workshops';
+import ratingsRouter from './ratings';
 
 const router = express.Router();
 
@@ -17,5 +19,7 @@ router.use('/users', usersRouter);
 router.use('/beneficiaries', beneficiariesRouter);
 router.use('/activities', activitiesRouter);
 router.use('/roles', rolesRouter);
+router.use('/workshops', workshopsRouter);
+router.use('/ratings', ratingsRouter);
 
 export default router;
