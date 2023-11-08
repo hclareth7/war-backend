e8573a7913f70da75766699858832503e7aaaa0f
hclareth7 <hclareth7@gmail.com>
2023-11-07 23:55:24 -0500

    adding suggestingItems in rating

--- src/models/rating.ts
+++ src/models/rating.ts
@@ -1,28 +1,37 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const ratingSchema = new mongoose.Schema({
+const ratingSchema = new mongoose.Schema(
+  {
     rating_type: {
-        type: String,
-        enum: ['Fisioterapia', 'Optometría','Psicología'],
-        required: true
+      type: String,
+      enum: ["Fisioterapia", "Optometría", "Psicología"],
+      required: true,
     },
     observations: {
-        type: String
+      type: String,
     },
-    diagnostic:{
-        type: String
+    diagnostic: {
+      type: String,
     },
-    author:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User'
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
     attendee: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Beneficiary',
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Beneficiary",
     },
-}, { timestamps: true });
-const mongoosePaginate = require('mongoose-paginate-v2');
-ratingSchema.plugin(mongoosePaginate)
-const Rating = mongoose.model('Rating', ratingSchema);
+    suggested_items: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Item",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+const mongoosePaginate = require("mongoose-paginate-v2");
+ratingSchema.plugin(mongoosePaginate);
+const Rating = mongoose.model("Rating", ratingSchema);
 
 export default Rating;
