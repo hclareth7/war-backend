f092115a37e5675975fc47de02661b538d816fb3
Hernando Clareth <hernando.ariza@ormuco.com>
2023-11-17 20:37:59 -0500

    fixing health regimen

--- src/models/beneficiary.ts
+++ src/models/beneficiary.ts
@@ -65,7 +65,7 @@ const beneficiarySchema = new mongoose.Schema({
     },
     health_regimen: {
         type: String,
-        enum: ['Subsidiado', 'Contributivo Cotizante', 'Contributivo Beneficiario', 'Régimen Especial', 'Retirado' ]
+        enum: ['Subsidiado', 'Contributivo Cotizante', 'Contributivo Beneficiario', 'Régimen Especial', 'Retirado', 'No Afiliado' ]
     },
     residence_department: {
         type: String
