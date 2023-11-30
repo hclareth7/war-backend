5b47e4fbf64f79f0ad10880611a5ebfa69441a1b
Hernando Clareth <hernando.ariza@ormuco.com>
2023-11-29 22:52:36 -0500

    por fin update bodega en esta MONDAAAAAAAAAAAAAAAAAA

--- src/controllers/pdfReport.ts
+++ src/controllers/pdfReport.ts
@@ -7,7 +7,7 @@ export const pdfTest = async (req, res, next) => {
     const page = await browser.newPage();
    
     await page.setContent('<h1 style="color: red">Hello, Puppeteer!</h1>');
-   
+    await page.setContent('<h1 style="color: red">Hello, Puppeteer!</h1>');
     await page.pdf({ path: 'example.pdf', format: 'A4' });
    
     await browser.close();
